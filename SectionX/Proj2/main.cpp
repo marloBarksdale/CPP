fafe1a8f7d481d73752354d0e903efb08449c36f
marloBarksdale <lyndenflood@gmail.com>
2023-01-29 18:15:55 -0500

    vs code config

---
+++ SectionX/Proj2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int num = 100;
+    cout << "Hello proj2" << endl;
+    cout << "Enter yout fav number: ";
+
+    cin >> num;
+
+    cout << "Your favorite number is " << num << "\n";
+    return 0;
+}
